3f3db8ec24c911a86d13fc6bf71d9fbd6b21f4c7
muvaaghoul <alexamatula@gmail.com>
2024-11-07 15:07:53 -0500

    Added classes

--- src/com/pluralsight/LineItem.java
+++ src/com/pluralsight/LineItem.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
 public class LineItem {
+    private int productId;
+    private String productName;
+    private double price;
+    private int quantity;
+ ... constructor getters and setters
+    public double getLineTotal(){
+        return quantity * price;
+    }
 }
